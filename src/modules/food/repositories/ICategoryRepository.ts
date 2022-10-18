352a6e79d72ddf9947f620cb22048fce1dd7ad1a
Davi Serrano <daviscardoso2901@gmail.com>
2022-10-18 15:08:22 -0300

    update ICategoryRepository

--- src/modules/food/repositories/ICategoryRepository.ts
+++ src/modules/food/repositories/ICategoryRepository.ts
@@ -3,6 +3,7 @@ import { CategoryFood } from "../model/category";
 interface ICategoryRepository {
     create(name: string): Promise<void>;
     findByName(name: string): Promise<CategoryFood>
+    findById(id: string): Promise<CategoryFood>
     findAll():Promise<CategoryFood[]>
     delete(name: string): Promise<void>;
 }
