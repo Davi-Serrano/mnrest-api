10d2710beb76e347d8f8e67942dd32255a19f2b9
Davi Serrano <daviscardoso2901@gmail.com>
2022-11-14 21:18:33 -0300

    testando front end

--- src/modules/user/useCases/authenticateUser/AuthenticaceUseController.ts
+++ src/modules/user/useCases/authenticateUser/AuthenticaceUseController.ts
@@ -10,6 +10,8 @@ class AuthenticaceUserController {
 
         const token =  await authenticaceUserUseCase.execute({ name, password})
 
+        console.log("alguem logou ");
+
         return res.json(token)
     }
 
